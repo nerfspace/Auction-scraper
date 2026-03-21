fc5a4015bd679f164bd12a9926b047809f8faa30
nerfspace <holcombzane@gmail.com>
2026-03-21 18:55:59 -0400

    Disable MongoDB connection and crypto imports
    Comment out MongoDB connection and crypto imports

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 'use strict';
 
-const crypto = require('crypto');
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://<username>:<password>@localhost:27017/<dbname>', { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('MongoDB connected!'))
-    .catch(err => console.error('MongoDB connection error:', err));
+//const crypto = require('crypto');
+//const mongoose = require('mongoose');
+//mongoose.connect('mongodb://<username>:<password>@localhost:27017/<dbname>', { useNewUrlParser: true, useUnifiedTopology: true })
+    //.then(() => console.log('MongoDB connected!'))
+    //.catch(err => console.error('MongoDB connection error:', err));
 
 const express = require('express');
 // ...rest of your server code
