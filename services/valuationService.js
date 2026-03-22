5271c3e9e726d64f9e7f7cf326dbe50e5e849fb4
nerfspace <holcombzane@gmail.com>
2026-03-21 20:16:21 -0400

    Change analyzeOpportunities to return all auction items
    Modified analyzeOpportunities to return all auction items instead of filtering for undervalued ones.

--- services/valuationService.js
+++ services/valuationService.js
@@ -4,13 +4,8 @@ class ValuationService {
     }
 
     analyzeOpportunities(auctionItems) {
-        return auctionItems.filter(item => this.isUndervalued(item));
-    }
-
-    isUndervalued(item) {
-        const currentBid = item.price?.value || 0;
-        const estimatedValue = item.estimatedValue || (currentBid * 1.5);
-        return currentBid < estimatedValue * 0.8;
+        // Return all items - let profit calculator determine viability
+        return auctionItems;
     }
 }
 
